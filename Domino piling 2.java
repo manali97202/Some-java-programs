0bc7bc936c98362b1a3a6c73851943b97fae98f3
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-05-10 20:33:34 +0530

    Add files via upload

---
+++ Domino piling 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
+
+Input:
+
+2 4
+Output:
+
+4
