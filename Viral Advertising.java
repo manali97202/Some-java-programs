60ab3784e726bc4fb89307305b3fa318a7e3cf43
Manali Sharma <manali.sharma_cs17@gla.ac.in>
2020-05-12 00:38:12 +0530

    Add files via upload

---
+++ Viral Advertising.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.lang.Math;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int k=5;
+        int s=0;
+        for(int i=1;i<=n;i++){
+           int l=(int)Math.floor(k/2);
+           s=s+l;
+           k=l*3;
+
+        }
+        return s;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+
+Input :
+3
+
+Output :
+9
